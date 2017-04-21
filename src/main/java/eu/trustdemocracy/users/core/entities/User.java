4f3993a3a1b003fc1bfd7564127ad72670076a22
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-22 00:24:27 +0200

    Pass createSingleUser test

--- src/main/java/eu/trustdemocracy/users/core/entities/User.java
+++ src/main/java/eu/trustdemocracy/users/core/entities/User.java
@@ -4,12 +4,43 @@ import java.util.UUID;
 
 public class User {
     private UUID id;
+    private String username;
+    private String email;
+    private String password;
 
     public UUID getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public User setId(UUID id) {
         this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
     }
 }
--- src/main/java/eu/trustdemocracy/users/core/interactors/CreateUser.java
+++ src/main/java/eu/trustdemocracy/users/core/interactors/CreateUser.java
@@ -1,15 +1,33 @@
 package eu.trustdemocracy.users.core.interactors;
 
+import eu.trustdemocracy.users.core.entities.User;
 import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
 import eu.trustdemocracy.users.core.models.response.UserResponseDTO;
 import eu.trustdemocracy.users.gateways.UserDAO;
 
 public class CreateUser {
+    private UserDAO userDAO;
 
     public CreateUser(UserDAO userDAO) {
+        this.userDAO = userDAO;
     }
 
     public UserResponseDTO execute(UserRequestDTO userRequestDTO) {
-        return null;
+        User user = userDAO.createUser(createUser(userRequestDTO));
+        return createUserResponse(user);
+    }
+
+    private User createUser(UserRequestDTO userRequestDTO) {
+        return new User()
+                .setUsername(userRequestDTO.getUsername())
+                .setEmail(userRequestDTO.getEmail())
+                .setPassword(userRequestDTO.getPassword());
+    }
+
+    private UserResponseDTO createUserResponse(User user) {
+        return new UserResponseDTO()
+                .setId(user.getId())
+                .setUsername(user.getUsername())
+                .setEmail(user.getEmail());
     }
 }
