2d0c2676b81a9baad943bc43fe6ef59dc92b2c80
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-21 21:01:27 +0200

    Change JUnit version

--- src/test/java/eu/trustdemocracy/users/AppTest.java
+++ src/test/java/eu/trustdemocracy/users/AppTest.java
@@ -1,33 +1,13 @@
 package eu.trustdemocracy.users;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
+    @Test
+    public void isAlwaysTrue() {
         assertTrue(true);
     }
 }
