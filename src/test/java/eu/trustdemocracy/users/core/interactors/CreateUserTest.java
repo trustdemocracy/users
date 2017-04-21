02e6847ae2796a220e564adb1faa0c6242e5b829
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-22 00:36:35 +0200

    Add createSeveralUsers test

--- src/test/java/eu/trustdemocracy/users/core/interactors/CreateUserTest.java
+++ src/test/java/eu/trustdemocracy/users/core/interactors/CreateUserTest.java
@@ -4,39 +4,66 @@ import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
 import eu.trustdemocracy.users.core.models.response.UserResponseDTO;
 import eu.trustdemocracy.users.gateways.UserDAO;
 import eu.trustdemocracy.users.gateways.fake.FakeUserDAO;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreateUserTest {
 
-    UserRequestDTO userRequestDTO;
+    static List<UserRequestDTO> inputUsers;
     UserDAO userDAO;
+
+    @BeforeAll
+    public static void initAll() {
+        inputUsers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            inputUsers.add(new UserRequestDTO()
+                    .setUsername("user" + i)
+                    .setEmail("user" + i + "@user.com")
+                    .setPassword("test" + i));
+        }
+    }
 
     @BeforeEach
     public void init() {
-        userRequestDTO = new UserRequestDTO()
-                .setUsername("user")
-                .setEmail("user@user.com")
-                .setPassword("test");
         userDAO = new FakeUserDAO();
     }
 
     @Test
     public void createSingleUser() {
         UUID uuid = userDAO.getUniqueUUID();
+        UserRequestDTO inputUser = inputUsers.get(0);
 
         UserResponseDTO expectedUser = new UserResponseDTO()
-                .setUsername("user")
-                .setEmail("user@user.com")
+                .setUsername(inputUser.getUsername())
+                .setEmail(inputUser.getEmail())
                 .setId(uuid);
 
         CreateUser interactor = new CreateUser(userDAO);
-        UserResponseDTO userResponseDTO = interactor.execute(userRequestDTO);
+        UserResponseDTO responseUser = interactor.execute(inputUser);
 
-        assertEquals(userResponseDTO, expectedUser);
+        assertEquals(responseUser, expectedUser);
+    }
+
+    @Test
+    public void createSeveralUsers() {
+        CreateUser interactor = new CreateUser(userDAO);
+
+        for (UserRequestDTO inputUser : inputUsers) {
+            UUID uuid = userDAO.getUniqueUUID();
+            UserResponseDTO expectedUser = new UserResponseDTO()
+                    .setUsername(inputUser.getUsername())
+                    .setEmail(inputUser.getEmail())
+                    .setId(uuid);
+            UserResponseDTO responseUser = interactor.execute(inputUser);
+
+            assertEquals(responseUser, expectedUser);
+        }
     }
 }
