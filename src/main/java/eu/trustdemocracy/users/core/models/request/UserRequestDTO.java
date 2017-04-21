57304d75473d192dfa2eee9f63b21afa7f4321ee
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-22 00:20:05 +0200

    Add equals, hashCode and toString to DTOs

--- src/main/java/eu/trustdemocracy/users/core/models/request/UserRequestDTO.java
+++ src/main/java/eu/trustdemocracy/users/core/models/request/UserRequestDTO.java
@@ -43,4 +43,36 @@ public class UserRequestDTO {
         this.password = password;
         return this;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRequestDTO that = (UserRequestDTO) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        return password != null ? password.equals(that.password) : that.password == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequestDTO{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/eu/trustdemocracy/users/core/models/response/UserResponseDTO.java
+++ src/main/java/eu/trustdemocracy/users/core/models/response/UserResponseDTO.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 public class UserResponseDTO {
     private UUID id;
     private String username;
+
     private String email;
 
     public UUID getId() {
@@ -32,5 +33,34 @@ public class UserResponseDTO {
     public UserResponseDTO setEmail(String email) {
         this.email = email;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserResponseDTO that = (UserResponseDTO) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        return email != null ? email.equals(that.email) : that.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponseDTO{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
