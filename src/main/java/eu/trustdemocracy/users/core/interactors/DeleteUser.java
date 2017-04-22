82344b4fa54dd04cd2b089de0a838d7aa8de81f5
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-23 01:39:31 +0200

    Add deleteSingleUser failing test

---
+++ src/main/java/eu/trustdemocracy/users/core/interactors/DeleteUser.java
@@ -0,0 +1,13 @@
+package eu.trustdemocracy.users.core.interactors;
+
+import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
+import eu.trustdemocracy.users.gateways.UserDAO;
+
+public class DeleteUser {
+
+  public DeleteUser(UserDAO userDAO) {
+  }
+
+  public void execute(UserRequestDTO inputUser) {
+  }
+}
---
+++ src/test/java/eu/trustdemocracy/users/core/interactors/DeleteUserTest.java
@@ -0,0 +1,47 @@
+package eu.trustdemocracy.users.core.interactors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
+import eu.trustdemocracy.users.core.models.response.UserResponseDTO;
+import eu.trustdemocracy.users.gateways.UserDAO;
+import eu.trustdemocracy.users.gateways.fake.FakeUserDAO;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DeleteUserTest {
+  private static Map<UUID, UserResponseDTO> responseUsers;
+  private UserDAO userDAO;
+
+  @BeforeEach
+  public void init() {
+    userDAO = new FakeUserDAO();
+    responseUsers = new HashMap<>();
+
+    CreateUser interactor = new CreateUser(userDAO);
+    for (int i = 0; i < 10; i++) {
+      UserRequestDTO inputUser = new UserRequestDTO()
+          .setUsername("user" + i)
+          .setEmail("user" + i + "@user.com")
+          .setPassword("test" + i)
+          .setName("Name" + i);
+
+      UserResponseDTO responseUser = interactor.execute(inputUser);
+      responseUsers.put(responseUser.getId(), responseUser);
+    }
+  }
+
+  @Test
+  public void deleteSingleUser() {
+    UserResponseDTO responseUser = responseUsers.values().iterator().next();
+    UserRequestDTO inputUser = new UserRequestDTO()
+        .setId(responseUser.getId());
+
+    new DeleteUser(userDAO).execute(inputUser);
+
+    assertEquals(null, userDAO.findById(responseUser.getId()));
+  }
+}
