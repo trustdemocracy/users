7e16d30a3edeae56641a6428a0986336dcfb88b6
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-25 00:53:18 +0200

    Add fake passing test

--- src/main/java/eu/trustdemocracy/users/endpoints/controllers/UserController.java
+++ src/main/java/eu/trustdemocracy/users/endpoints/controllers/UserController.java
@@ -1,7 +1,12 @@
 package eu.trustdemocracy.users.endpoints.controllers;
 
+import eu.trustdemocracy.users.core.entities.utils.UserMapper;
+import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
+import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import java.util.UUID;
+import lombok.val;
 
 public class UserController extends Controller {
 
@@ -12,9 +17,20 @@ public class UserController extends Controller {
   @Override
   public void buildRoutes() {
     router.get("/").handler(this::handleProposals);
+    router.post("/users").handler(this::createUser);
   }
 
   private void handleProposals(RoutingContext routingContext) {
     routingContext.response().putHeader("content-type", "application/json").end("{'status': 'ok'}");
+  }
+
+  private void createUser(RoutingContext routingContext) {
+    val requestUser = Json.decodeValue(routingContext.getBodyAsString(), UserRequestDTO.class);
+    val user = UserMapper.createEntity(requestUser).setId(UUID.randomUUID());
+
+    routingContext.response()
+        .putHeader("content-type", "application/json")
+        .setStatusCode(201)
+        .end(Json.encodePrettily(UserMapper.createResponse(user)));
   }
 }
--- src/test/java/eu/trustdemocracy/users/endpoints/UserControllerTest.java
+++ src/test/java/eu/trustdemocracy/users/endpoints/UserControllerTest.java
@@ -1,8 +1,11 @@
 package eu.trustdemocracy.users.endpoints;
 
 
+import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
+import eu.trustdemocracy.users.core.models.response.UserResponseDTO;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -16,6 +19,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
 public class UserControllerTest {
+
+  private static final String HOST = "localhost";
 
   private Vertx vertx;
   private Integer port;
@@ -42,11 +47,52 @@ public class UserControllerTest {
   public void testMyApplication(TestContext context) {
     val async = context.async();
 
-    vertx.createHttpClient().getNow(port, "localhost", "/", response -> {
+    vertx.createHttpClient().getNow(port, HOST, "/", response -> {
       response.handler(body -> {
         context.assertTrue(body.toString().contains("status"));
         async.complete();
       });
     });
+  }
+
+  @Test
+  public void createUser(TestContext context) {
+    val async = context.async();
+
+    val userRequest = new UserRequestDTO()
+        .setUsername("test")
+        .setEmail("test@test.com")
+        .setPassword("password")
+        .setName("TestName")
+        .setSurname("TestSurname");
+
+    val jsonUser = Json.encodePrettily(userRequest);
+    val length = Integer.toString(jsonUser.length());
+
+
+    vertx.createHttpClient().post(port, HOST, "/users")
+        .putHeader("content-type", "application/json")
+        .putHeader("content-length", length)
+        .handler(response -> {
+          context.assertEquals(response.statusCode(), 201);
+          context.assertTrue(response.headers().get("content-type").contains("application/json"));
+
+          response.bodyHandler(body -> {
+            try {
+              val responseUser = Json.decodeValue(body.toString(), UserResponseDTO.class);
+              context.assertEquals(responseUser.getUsername(), userRequest.getUsername());
+              context.assertEquals(responseUser.getEmail(), userRequest.getEmail());
+              context.assertEquals(responseUser.getName(), userRequest.getName());
+              context.assertEquals(responseUser.getSurname(), userRequest.getSurname());
+              context.assertNotNull(responseUser.getId());
+            } catch (Exception e) {
+              context.fail(e);
+            } finally {
+              async.complete();
+            }
+          });
+        })
+        .write(jsonUser)
+        .end();
   }
 }
