06c2d3ad730c2d39c65855ba591279b9f3395a48
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-17 02:16:22 +0200

    Pass storeRefreshToken test

--- src/main/java/eu/trustdemocracy/users/gateways/mongo/MongoTokenDAO.java
+++ src/main/java/eu/trustdemocracy/users/gateways/mongo/MongoTokenDAO.java
@@ -1,18 +1,27 @@
 package eu.trustdemocracy.users.gateways.mongo;
 
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import eu.trustdemocracy.users.gateways.TokenDAO;
 import java.util.UUID;
+import lombok.val;
+import org.bson.Document;
 
 public class MongoTokenDAO implements TokenDAO {
 
-  public MongoTokenDAO(MongoDatabase database) {
+  private static final String TOKENS_COLLECTION = "tokens";
+  private MongoCollection<Document> collection;
 
+  public MongoTokenDAO(MongoDatabase db) {
+    this.collection = db.getCollection(TOKENS_COLLECTION);
   }
 
   @Override
   public void storeRefreshToken(UUID userId, String refreshToken) {
+    val document = new Document("id", userId.toString())
+        .append("refreshToken", refreshToken);
 
+    collection.insertOne(document);
   }
 
   @Override
