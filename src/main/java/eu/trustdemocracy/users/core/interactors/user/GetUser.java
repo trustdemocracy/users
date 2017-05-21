cd3b828f63cf667a214a6d40d707586e214b05ff
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-21 02:26:49 +0200

    Pass getUserByUsername test

--- src/main/java/eu/trustdemocracy/users/core/interactors/user/GetUser.java
+++ src/main/java/eu/trustdemocracy/users/core/interactors/user/GetUser.java
@@ -1,11 +1,11 @@
 package eu.trustdemocracy.users.core.interactors.user;
 
+import eu.trustdemocracy.users.core.entities.User;
 import eu.trustdemocracy.users.core.entities.util.UserMapper;
 import eu.trustdemocracy.users.core.interactors.UserInteractor;
 import eu.trustdemocracy.users.core.models.request.UserRequestDTO;
 import eu.trustdemocracy.users.core.models.response.UserResponseDTO;
 import eu.trustdemocracy.users.gateways.UserDAO;
-import lombok.val;
 
 public class GetUser extends UserInteractor {
 
@@ -14,7 +14,13 @@ public class GetUser extends UserInteractor {
   }
 
   public UserResponseDTO execute(UserRequestDTO inputUser) {
-    val user = userDAO.findById(inputUser.getId());
+    User user;
+    if (inputUser.getId() == null) {
+      user = userDAO.findByUsername(inputUser.getUsername());
+
+    } else {
+      user = userDAO.findById(inputUser.getId());
+    }
     return UserMapper.createResponse(user);
   }
 }
